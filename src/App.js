47c2c7bd025ca1d2d91d46a9f8869e14488f63d2
SuJin <kimsujin001127@gmail.com>
2024-01-09 17:34:40 +0900

    fix: router 설정 변경

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Ranking from "./pages/Ranking";
 import MyPage from "./pages/MyPage";
 
-const route = createBrowserRouter([
-  {
-    path: "/",
-    element: <Outlet />,
-    children: [
-      {
-        path: "home",
-        element: <Home />,
-        children: [],
-      },
-      {
-        path: "ranking",
-        element: <Ranking />,
-        children: [],
-      },
-      {
-        path: "mypage",
-        element: <MyPage />,
-        children: [],
-      },
-    ],
-  },
-]);
-
 function App() {
   return (
-    <>
-      <RouterProvider router={route} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ranking" element={<Ranking />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
