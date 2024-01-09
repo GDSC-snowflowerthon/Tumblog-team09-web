f21251e0f59276bcb699e07c489547af6291210f
SuJin <kimsujin001127@gmail.com>
2024-01-09 17:38:04 +0900

    feat: 네비게이션바 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Ranking from "./pages/Ranking";
 import MyPage from "./pages/MyPage";
+import Navigator from "./components/Navigator/Navigator";
+import styled from "styled-components";
 
 function App() {
   return (
@@ -11,8 +13,16 @@ function App() {
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
+      <NavWrapper>
+        <Navigator />
+      </NavWrapper>
     </BrowserRouter>
   );
 }
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default App;
---
+++ src/components/Navigator/Navigator.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import HomeIcon from "../../images/nav/home.svg";
+import MypageIcon from "../../images/nav/mypage.svg";
+import RankingIcon from "../../images/nav/ranking.svg";
+import COLORS from "../../styles/colors";
+import { useNavigate } from "react-router-dom";
+
+const Navigator = () => {
+  const navigate = useNavigate();
+  return (
+    <FixedBox>
+      <Container>
+        <MenuBox onClick={() => navigate("/ranking")}>
+          <img alt="rank" src={RankingIcon} />
+        </MenuBox>
+        <MenuBox onClick={() => navigate("/")}>
+          <img alt="home" src={HomeIcon} />
+        </MenuBox>
+        <MenuBox onClick={() => navigate("/mypage")}>
+          <img alt="mypage" src={MypageIcon} />
+        </MenuBox>
+      </Container>
+    </FixedBox>
+  );
+};
+
+const FixedBox = styled.div`
+  position: fixed;
+  bottom: 0px;
+  z-index: 5;
+  min-width: 25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 100%;
+  height: 72px;
+  border-top: 1px solid ${COLORS.gray};
+`;
+
+const MenuBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30vw;
+`;
+
+export default Navigator;
